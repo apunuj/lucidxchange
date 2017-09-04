3b0d10f9668548e7bbd80e511310a785ccd9f7e0
Apoorv Kesharwani <kesharwani.apoorv@gmail.com>
2017-09-04 11:05:19 +0530

    Built the project for static deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
+const path = require('path');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: 'build/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['.js']
